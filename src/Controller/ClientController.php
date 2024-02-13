0db2f5fdb02ba323deabefde4b7c79d1de170e63
GuillemotClement <guillemot.clement@protonmail.com>
2024-02-13 17:26:36 +0100

    Ajout du crud from scratch de client

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
+use App\Form\ClientType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Entity;
 use App\Repository\ClientRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ClientController extends AbstractController
 {
@@ -20,6 +26,36 @@ class ClientController extends AbstractController
         return $this->render('client/index.html.twig', [
             'clients'=>$clients,
             'nbre'=>count($clients),
+        ]);
+    }
+
+    #[Route('/client/delete/{id}', name: 'app_client_delete')]
+    public function delete(EntityManagerInterface $em, $id){
+        $client = $em->getRepository(Client::class)->find($id);
+        $em->remove($client);
+        $em->flush();
+        return $this->redirectToRoute("app_client");
+    }
+    //création d'une route pour la fonction
+    //chemin qui correspond à l'élément à modifier
+    #[Route('/client/edit/{id}', name:"app_client_edit", methods:["POST", "GET"])]
+    public function edit(EntityManagerInterface $em, ClientRepository $cr, $id, Request $request){
+        $id = (int)$id;
+        if($id){
+            $client = $cr->find($id);
+        }else{
+            $client = new Client();
+        }
+        //création du form à partir de ClientType sur l'entity Client = $client
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($client); //fournit les données $client 
+            $em->flush(); //enregistrer les données saisie dans la bdd
+            return $this->redirectToRoute("app_client");
+        }
+        return $this->render("client/form.html.twig",[
+            'form'=>$form->createView(),
         ]);
     }
 }
